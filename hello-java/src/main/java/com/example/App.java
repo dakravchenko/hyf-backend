e91244a6aa2323b125bf4913941adc9835b60536
Daniil Kravchenko <daniil.kravchenko@gmail.com>
2026-05-07 19:05:18 +0200

    Add hello-java Maven project

---
+++ hello-java/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int localValue = 5 + 10;
+        System.out.println( "Hello! My name is Daniil and I'm from Amsterdam." );
+    }
+}
